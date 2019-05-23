3f2b99d2710796f18ff7f2fc6fe3df22d63a7c05
coreyclark.business@gmail.com <apollo@Fingertip-Island.localdomain>
2019-05-22 19:22:53 -0700

    Added search method for the ternary search tree object. You can now search for a list of strings and it will return either true (if your list is inside the tree) or false (if it's not)

--- ternary_search.py
+++ ternary_search.py
@@ -11,37 +11,33 @@ class TernTree:
     def __init__(self):
         self.root_node = None
 
-    def __iter__(self):
-        for attr, value in self.__dict__.items():
-            return attr, value
-
     @staticmethod
     def get_node(char):
         new_node = Node()
         new_node.char = char
         return new_node
 
-    def insert(self, node, string):
-        if len(string) == 0:
+    def insert(self, node, key):
+        if len(key) == 0:
             node.mid = self.get_node(None)
             node.mid.is_end = True
             return node
 
-        head = string[0]
-        tail = string[1:]
+        head = key[0]
+        tail = key[1:]
 
         if node.char is None:
             node.char = head
             self.insert(node, tail)
         elif head < node.char and node.mid is not None:
             if node.left is not None:
-                self.insert(node.left, string)
+                self.insert(node.left, key)
             else:
                 node.left = self.get_node(head)
                 self.insert(node.left, tail)
         elif head > node.char and node.mid is not None:
             if node.right is not None:
-                self.insert(node.right, string)
+                self.insert(node.right, key)
             else:
                 node.right = self.get_node(head)
                 self.insert(node.right, tail)
@@ -54,25 +50,51 @@ class TernTree:
 
         self.root_node = node
 
-    ########## TO-DO ##########
+    def search(self, node: list or Node, key: str):
+        if len(key) == 0:
+            if node.is_end:
+                return True
+            else:
+                return False
+        elif node.char is None or node is None:
+            return False
+
+        head = key[0]
+        tail = key[1:]
+
+        if node.char == head:
+            return self.search(node.mid, tail)
+        elif head < node.char and node.left is not None:
+            return self.search(node.left, key)
+        elif head > node.char and node.right is not None:
+            return self.search(node.right, key)
+        else:
+            return False
+
     def to_string(self, node):
         tree_nodes = {}
         for k, v in node.__dict__.items():
+            tree_nodes[k] = v
             tree_nodes[k] = v
             if isinstance(v, Node):
                 tree_nodes[k] = self.to_string(v)
         return tree_nodes
 
-    def search(self, node, key):
-        return None
+    #### TO_DO ####
+    # - Import/export tree to json
+    # - Delete
 
 
 tree = TernTree()
 root = Node()
-tree_list = ['eric', 'ericah', 'ericcc', 'ericcon', 'ericahn', 'mike', 'able', 'ableton', 'abernathy']
+tree_list = ['eric', 'mike', 'able', 'aria', 'bri', 'abby', 'erica', 'erin']
+search_list = ['jared', 'eric', 'ariana', 'aria', 'arias', 'able', 'ericah', 'erin']
 
 for i in tree_list:
     tree.insert(root, i)
-# print(tree.root_node)
-# print(tree.root_node.to_string())
-print(tree.to_string(tree.root_node))
+
+for i in search_list:
+    if tree.search(tree.root_node, i):
+        print('true')
+    elif not tree.search(tree.root_node, i):
+        print('false')
